9fa7424a03ba8830e00635eb20bbc63505981f35
JacChin <155051742+JacChin@users.noreply.github.com>
2023-12-28 15:01:06 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,71 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import joblib
+import numpy as np
 
-"""
-# Welcome to Streamlit!
+# Load the trained model
+model = joblib.load('best_dt_model.joblib')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Streamlit App
+st.title('Diabetes Detection Using Machine Learning')
+st.sidebar.header('User Input Features')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Create input features (adjust based on your model's features)
+# Assuming your model expects features named 'Age' and 'BMI'
+age = st.sidebar.slider('Age', min(df['Age']), max(df['Age']), df['Age'].mean())
+bmi = st.sidebar.slider('BMI', min(df['BMI']), max(df['BMI']), df['BMI'].mean())
+sex = st.sidebar.slider('Sex', min(df['Sex']), max(df['Sex']), df['Sex'].mean())
+# You can add more features based on your DataFrame columns
+education = st.sidebar.slider('Education', min(df['Education']), max(df['Education']), df['Education'].mean())
+income = st.sidebar.slider('Income', min(df['Income']), max(df['Income']), df['Income'].mean())
+highBP = st.sidebar.slider('HighBP', min(df['HighBP']), max(df['HighBP']), df['HighBP'].mean())
+highchol = st.sidebar.slider('HighChol', min(df['HighChol']), max(df['HighChol']), df['HighChol'].mean())
+cholCheck = st.sidebar.slider('CholCheck', min(df['CholCheck']), max(df['CholCheck']), df['CholCheck'].mean())
+smoker = st.sidebar.slider('Smoker', min(df['Smoker']), max(df['Smoker']), df['Smoker'].mean())
+stroke = st.sidebar.slider('Stroke', min(df['Stroke']), max(df['Stroke']), df['Stroke'].mean())
+heartDiseaseorAttack = st.sidebar.slider('HeartDiseaseorAttack', min(df['HeartDiseaseorAttack']), max(df['HeartDiseaseorAttack']), df['HeartDiseaseorAttack'].mean())
+physActivity = st.sidebar.slider('PhysActivity', min(df['PhysActivity']), max(df['PhysActivity']), df['PhysActivity'].mean())
+fruits = st.sidebar.slider('Fruits', min(df['Fruits']), max(df['Fruits']), df['Fruits'].mean())
+veggies = st.sidebar.slider('Veggies', min(df['Veggies']), max(df['Veggies']), df['Veggies'].mean())
+hvyAlcoholConsump = st.sidebar.slider('HvyAlcoholConsump', min(df['HvyAlcoholConsump']), max(df['HvyAlcoholConsump']), df['HvyAlcoholConsump'].mean())
+anyHealthcare = st.sidebar.slider('AnyHealthcare', min(df['AnyHealthcare']), max(df['AnyHealthcare']), df['AnyHealthcare'].mean())
+noDocbcCost = st.sidebar.slider('NoDocbcCost', min(df['NoDocbcCost']), max(df['NoDocbcCost']), df['NoDocbcCost'].mean())
+genHlth = st.sidebar.slider('GenHlth', min(df['GenHlth']), max(df['GenHlth']), df['GenHlth'].mean())
+mentHlth = st.sidebar.slider('MentHlth', min(df['MentHlth']), max(df['MentHlth']), df['MentHlth'].mean())
+physHlth = st.sidebar.slider('PhysHlth', min(df['PhysHlth']), max(df['PhysHlth']), df['PhysHlth'].mean())
+diffWalk = st.sidebar.slider('DiffWalk', min(df['DiffWalk']), max(df['DiffWalk']), df['DiffWalk'].mean())
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+# Convert input data to array
+input_data = pd.DataFrame({
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
+    'HighBP': [highBP],
+    'HighChol': [highchol],
+    'CholCheck': [cholCheck],
+    'BMI': [bmi],
+    'Smoker': [smoker],
+    'Stroke': [stroke],
+    'HeartDiseaseorAttack': [heartDiseaseorAttack],
+    'PhysActivity': [physActivity],
+    'Fruits': [fruits],
+    'Veggies': [veggies],
+    'HvyAlcoholConsump': [hvyAlcoholConsump],
+    'AnyHealthcare': [anyHealthcare],
+    'NoDocbcCost': [noDocbcCost],
+    'GenHlth': [genHlth],
+    'MentHlth': [mentHlth],
+    'PhysHlth': [physHlth],
+    'DiffWalk': [diffWalk],
+    'Sex': [sex],
+    'Age': [age],
+    'Education': [education],
+    'Income': [income],
+    
 })
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+
+# Make predictions
+prediction = model.predict(input_data)
+
+# Display predictions
+st.subheader('Prediction')
+st.write(prediction)
